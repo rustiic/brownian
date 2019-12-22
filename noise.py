21b3e9c884a74662a01279f212d13b9586dd5114
Shashank <hexagonad@disroot.org>
2019-12-22 11:18:03 +0530

    The definitve version of the code that calculates the moments for random and SystemRandom numbers. Removed unecessary errors and put everything into a function.

--- noise.py
+++ noise.py
@@ -3,48 +3,50 @@ import numpy as np
 from random import SystemRandom, random, randint
 import matplotlib.pyplot as plt
 
+def Sample(size, moment):
+    # size specifies the size of the arrays
+    # moment specifies the # of moments to be calculated
 
-size = 1000   #Specifies the size of the arrays
-moment = 5
+    cryptogen = SystemRandom()
+    randon, crypton = np.zeros((moment), dtype=float), np.zeros((moment), dtype=float)
+    #silly = np.zeros(moment, dtype=float)
 
-cryptogen = SystemRandom()
-randon, crypton = np.zeros(moment, dtype=float), np.zeros(moment, dtype=float)
-#silly = np.zeros(moment, dtype=float)
+    def rando():
+        return randint(0, 1000)
 
-def rando():
-    return randint(0, 1000)
-
-def crypto():
-    return cryptogen.randint(0, 1000)
-
-
-X = np.zeros(size, dtype=float)
-Y = np.zeros(size, dtype=float)
-#Z = np.zeros(size, dtype=float)
+    def crypto():
+        return cryptogen.randint(0, 1000)
 
 
-for i in range(size):
-    x = rando()
-    y = crypto()
-    #z = randint(500, 999)
-    X[x-1] += 1/size
-    Y[y-1] += 1/size
-    #Z[z-1] += 1/size
+    X = np.zeros(size, dtype=float)
+    Y = np.zeros(size, dtype=float)
+    # rand_moments = np.zeros(size, dtype=float)
+    # crypt_moments = np.zeros(size, dtype=float)
+    #Z = np.zeros(size, dtype=float)
+
+    for i in range(size):
+        x = rando()
+        y = crypto()
+        #z = randint(500, 999)
+        X[x-1] += 1/size
+        Y[y-1] += 1/size
+        #Z[z-1] += 1/size
 
 
-for i in range(moment):
-    for j in range(size):
-        randon[i]  += (j**(i+1))*X[j]
-        crypton[i] += (j**(i+1))*Y[j]
+    for i in range(moment):
+        for j in range(size):
+            randon[i] += j**(i+1)*X[j]
+            crypton[i] += j**(i+1)*Y[j]
 
-#'''
-f, axarr = plt.subplots(2, sharex=True)
-axarr[0].plot(X, 'b-')
-axarr[0].set_title('random.random()')
-axarr[1].plot(Y, 'r-')
-axarr[1].set_title('SystemRandom')
-plt.savefig('plot.png')
-#'''
+    
+    # f, axarr = plt.subplots(2, sharex=True)
+    # axarr[0].plot(X, 'b-')
+    # axarr[0].set_title('random.random()')
+    # axarr[1].plot(Y, 'r-')
+    # axarr[1].set_title('SystemRandom')
+    # plt.savefig('plot.png')
 
-for i in range(moment):
-    print("\n {}th moment of \n Rando : {} \n Crypto: {}".format( i+1, randon[i], crypton[i]))
+    for i in range(moment):
+        print("\n {}th moment of \n Rando : {} \n Crypto: {}".format( i+1, randon[i], crypton[i]))
+
+Sample(1000, 20)
